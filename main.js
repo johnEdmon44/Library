ecf48f8cf2377f9d3859d3e848e318f379418108
Nessaj44 <johnedmoncoc@gmail.com>
2022-09-28 23:11:23 +0800

    Add a book with user input
    Creating a new Book object then push it to myLibrary array

--- main.js
+++ main.js
@@ -2,13 +2,42 @@
 
 const modal = document.querySelector('#modal');
 const showModal = document.querySelector('#showModal');
-const closeModal = document.querySelector('#close')
+const closeModal = document.querySelector('#close');
+
 
 showModal.addEventListener('click', function() {
   modal.classList.add('modal');
 });
 
 closeModal.addEventListener('click', function() {
-  closeModal.required = false;
   modal.classList.remove('modal');
-})
+}); 
+
+
+let myLibrary = [];
+
+function Book(title, author, pages, status) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+}
+
+function addBook() {
+  myLibrary.push(new Book(
+    document.querySelector('#title').value,
+    document.querySelector('#author').value,
+    document.querySelector('#pages').value,
+    isRead()
+  ));
+  modal.classList.remove('modal');
+}
+
+function isRead() {
+  if(document.querySelector('#isRead').checked == true) {
+    return 'read';
+  } else {
+    return 'not read';
+  }
+}
+
