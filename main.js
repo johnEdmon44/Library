e291e47b8f908f3f29e80b52fd77c3288e37395b
Nessaj44 <johnedmoncoc@gmail.com>
2022-09-28 09:14:16 +0800

    Add modal

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-"use strict"
+"use strict";
+
+const modal = document.querySelector('#modal');
+const showModal = document.querySelector('#showModal');
+const closeModal = document.querySelector('#close')
+
+showModal.addEventListener('click', function() {
+  modal.classList.add('modal');
+});
+
+closeModal.addEventListener('click', function() {
+  closeModal.required = false;
+  modal.classList.remove('modal');
+})
